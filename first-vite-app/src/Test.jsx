242334dafdb229d203b752a60df2a9f2152b1063
unknown <dashulja861@mail.com>
2024-02-20 08:13:31 +0000

    first commit

---
+++ first-vite-app/src/Test.jsx
@@ -0,0 +1,10 @@
+import logo from './assets/react.svg'
+export function Test(){
+    return(
+        <>
+            <h2>Hello from Test!</h2>
+            <p>How are you?</p>
+            <img className="logo" src={logo} alt="logo" />
+        </>
+    )
+}
